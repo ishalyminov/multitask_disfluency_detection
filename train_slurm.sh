f420805b35b7d57480c57b4887790b91489c1e64
Igor Shalyminov <ishalyminov@gmail.com>
2017-10-17 14:01:01 +0100

    slurm script added

---
+++ train_slurm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --partition=amd-longq
+#SBATCH --nodes 1
+#SBATCH --gres=gpu
+#SBATCH --mail-user=is33@hw.ac.uk
+#SBATCH --mail-type=ALL
+
+# do some stuff to setup the environment
+module purge
+module load shared
+module load cuda80/blas/8.0.44 cuda80/fft/8.0.44 cuda80/gdk/352.79 cuda80/nsight/8.0.44 cuda80/profiler/8.0.44 cuda80/toolkit/8.0.44 cudnn/5.1
+
+# execute application (read in arguments from command line)
+cd /home/ishalyminov/data/dialogue_denoiser && /home/ishalyminov/Envs/dialogue_denoiser/bin/python -m dialogue_denoiser \
+  --train \
+  --data_dir ../babi_tools/dialogue_denoiser_data \
+  --train_dir ckpt \
+  --from_train_data ../babi_tools/dialogue_denoiser_data/dialog-babi-task1-API-calls-trn.txt/encoder.txt \
+  --to_train_data ../babi_tools/dialogue_denoiser_data/dialog-babi-task1-API-calls-trn.txt/decoder.txt \
+  --from_dev_data ../babi_tools/dialogue_denoiser_data/dialog-babi-task1-API-calls-dev.txt/encoder.txt \                       
+  --to_dev_data ../babi_tools/dialogue_denoiser_data/dialog-babi-task1-API-calls-dev.txt/decoder.txt
+
+# exit
+exit 0
