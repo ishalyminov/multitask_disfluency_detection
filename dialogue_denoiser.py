470c8d3951594cf9442149fc89fac8a9808aeb97
Igor Shalyminov <ishalyminov@gmail.com>
2018-02-01 14:31:29 +0000

    word dropout, early stopping threshold implemented

--- dialogue_denoiser.py
+++ dialogue_denoiser.py
@@ -43,9 +43,13 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 from copy_seq2seq import data_utils, seq2seq_model
 
-tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate", 0.1, "Learning rate.")
 tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99, "Learning rate decays by this much.")
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm.")
+tf.app.flags.DEFINE_float("word_dropout_prob", 0.0, "Word dropout probability during training")
+tf.app.flags.DEFINE_float("early_stopping_threshold",
+                          0.01,
+                          "Loss decreasing less than this (relatively) will cause early stopping")
 tf.app.flags.DEFINE_integer("batch_size", 16, "Batch size to use during training.")  # 8
 tf.app.flags.DEFINE_integer("size", 64, "Size of each model layer.")  # 32
 tf.app.flags.DEFINE_integer("num_layers", 1, "Number of layers in the model.")
@@ -182,13 +186,14 @@ def train():
     from_dev, to_dev = dev_data
     from_test, to_test = test_data
 
-    train_tokenized = [(from_line, to_line)
+    encoder_size, decoder_size = _buckets[-1]
+    train_tokenized = [(from_line + [data_utils.PAD_ID] * (encoder_size - len(from_line)), to_line)
                        for from_line, to_line in zip(data_utils.tokenize_data(from_train_data),
                                                      data_utils.tokenize_data(to_train_data))]
-    dev_tokenized = [(from_line, to_line)
+    dev_tokenized = [(from_line + [data_utils.PAD_ID] * (encoder_size - len(from_line)), to_line)
                      for from_line, to_line in zip(data_utils.tokenize_data(from_dev_data),
                                                    data_utils.tokenize_data(to_dev_data))]
-    test_tokenized = [(from_line, to_line)
+    test_tokenized = [(from_line + [data_utils.PAD_ID] * (encoder_size - len(from_line)), to_line)
                       for from_line, to_line in zip(data_utils.tokenize_data(from_test_data),
                                                     data_utils.tokenize_data(to_test_data))]
 
@@ -238,7 +243,7 @@ def train():
 
             # Get a batch and make a step.
             encoder_inputs, decoder_inputs, decoder_targets, target_weights = \
-                model.get_batch(train_set, bucket_id)
+                model.get_batch(train_set, bucket_id, word_dropout_prob=FLAGS.word_dropout_prob)
             _, step_loss, _ = model.step(sess,
                                          encoder_inputs,
                                          decoder_inputs,
@@ -277,32 +282,27 @@ def train():
                                                                        rev_dec_vocab,
                                                                        test_set,
                                                                        test_tokenized)
-                print("  train: loss %.2f perplexity %.2f per-utterance accuracy %.2f" % (
-                    train_loss,
-                    train_perplexity,
-                    train_accuracy))
-                print("  dev: loss %.2f perplexity %.2f per-utterance accuracy %.2f" % (
-                    dev_loss,
-                    dev_perplexity,
-                    dev_accuracy))
-                print("  test: loss %.2f perplexity %.2f per-utterance accuracy %.2f" % (
-                    test_loss,
-                    test_perplexity,
-                    test_accuracy))
+                print("  train: loss %.2f perplexity %.2f per-utterance accuracy %.2f"
+                      % (train_loss, train_perplexity, train_accuracy))
+                print("  dev: loss %.2f perplexity %.2f per-utterance accuracy %.2f"
+                      % (dev_loss, dev_perplexity, dev_accuracy))
+                print("  test: loss %.2f perplexity %.2f per-utterance accuracy %.2f"
+                      % (test_loss, test_perplexity, test_accuracy))
                 sys.stdout.flush()
-                if best_dev_loss is None or dev_loss < best_dev_loss:
-                    suboptimal_loss_steps = 0
-                    best_train_loss, best_dev_loss, best_test_loss = train_loss, dev_loss, test_loss
-                    best_loss_step = model.global_step
-                    # Save checkpoint and zero timer and loss.
-                    model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+
+                if best_dev_loss is None or FLAGS.early_stopping_threshold < (best_dev_loss - dev_loss) / (best_dev_loss + 1e-12):
+                  suboptimal_loss_steps = 0
+                  best_train_loss, best_dev_loss, best_test_loss = train_loss, dev_loss, test_loss
+                  best_loss_step = model.global_step
+                  # Save checkpoint and zero timer and loss.
+                  model.saver.save(sess, checkpoint_path, global_step=model.global_step)
                 else:
-                    suboptimal_loss_steps += 1
-                    if FLAGS.early_stopping_checkpoints <= suboptimal_loss_steps:
-                        print("Early stopping after %d checkpoints" % FLAGS.early_stopping_checkpoints)
-                        break
-        print("Best loss achieved: %.2f (train) %.2f (*dev) %.2f (test)" %
-              (best_train_loss, best_dev_loss, best_test_loss))
+                  suboptimal_loss_steps += 1
+                  if FLAGS.early_stopping_checkpoints <= suboptimal_loss_steps:
+                    print("Early stopping after %d checkpoints" % FLAGS.early_stopping_checkpoints)
+                    break
+        print("Best loss achieved: %.2f (train) %.2f (*dev) %.2f (test)"
+              % (best_train_loss, best_dev_loss, best_test_loss))
 
 
 def eval_model(in_session, in_model, in_rev_dec_vocab, dataset, in_dataset_tokenized):
@@ -426,8 +426,9 @@ def evaluate():
         fr_vocab, rev_fr_vocab = data_utils.initialize_vocabulary(fr_vocab_path)
         model = create_model(sess, len(en_vocab), len(fr_vocab), forward_only=True)
 
+        encoder_size, decoder_size = _buckets[-1]
         dev_set = read_data(from_dev, to_dev)
-        dev_tokenized = [(from_line, to_line)
+        dev_tokenized = [(from_line + [data_utils.PAD_ID] * (encoder_size - len(from_line)), to_line)
                          for from_line, to_line in zip(data_utils.tokenize_data(from_dev_path),
                                                        data_utils.tokenize_data(to_dev_path))]
         loss, perplexity, accuracy = eval_model(sess, model, rev_fr_vocab, dev_set, dev_tokenized)
