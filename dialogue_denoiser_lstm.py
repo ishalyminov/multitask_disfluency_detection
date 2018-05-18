d730ef14e7328546bee4311ffebb4b19c0e9524b
Igor Shalyminov <ishalyminov@gmail.com>
2018-05-18 01:32:55 +0100

    fixes

--- dialogue_denoiser_lstm.py
+++ dialogue_denoiser_lstm.py
@@ -493,6 +493,161 @@ def eval_deep_disfluency(in_model,
             'f1_<e_word': all_results['f1_<e_word']}
 
 
+def create_fake_timings(in_tokens_number):
+    return list(zip(map(float, range(0, in_tokens_number)),
+                    map(float, range(1, in_tokens_number + 1))))
+
+
+def predict_babi_file(in_model,
+                      in_vocab,
+                      in_label_vocab,
+                      in_rev_label_vocab,
+                      dataset,
+                      in_config,
+                      in_session,
+                      target_file_path=None):
+    if target_file_path:
+        target_file = open(target_file_path, "w")
+
+    # eval tags --> RNN tags
+    X, y = make_dataset(dataset,
+                        in_vocab,
+                        in_label_vocab,
+                        in_config)
+    predictions = predict(in_model,
+                          (X, y),
+                          in_rev_label_vocab,
+                          in_session)
+    predictions_eval = []
+    global_word_index = 0
+    broken_sequences_number = 0
+    # RNN tags --> eval tags
+    for utterance in dataset['utterance']:
+        current_tags = predictions[global_word_index: global_word_index + len(utterance)]
+        try:
+            current_tags_eval = convert_from_inc_disfluency_tags_to_eval_tags(current_tags,
+                                                                              utterance,
+                                                                              representation="disf1")
+        except:
+            current_tags_eval = current_tags
+            broken_sequences_number += 1
+        predictions_eval += current_tags_eval
+        global_word_index += len(utterance)
+    print '#broken sequences after RNN --> eval conversion: {} out of {}'.format(broken_sequences_number,
+                                                                                 dataset.shape[0])
+
+    predictions_eval_iter = iter(predictions_eval)
+    for speaker, (_, speaker_data) in enumerate(dataset.iterrows()):
+        if target_file_path:
+            target_file.write("Speaker: " + str(speaker) + "\n\n")
+        timing_data, lex_data, pos_data, labels = (create_fake_timings(len(speaker_data['utterance'])),
+                                                   speaker_data['utterance'],
+                                                   speaker_data['pos'],
+                                                   speaker_data['tags'])
+
+        for i in range(0, len(timing_data)):
+            # print i, timing_data[i]
+            _, end = timing_data[i]
+            word = lex_data[i]
+            pos = pos_data[i]
+            predicted_tags = [next(predictions_eval_iter)]
+            current_time = end
+            if target_file_path:
+                target_file.write("Time: " + str(current_time) + "\n")
+                new_words = lex_data[i - (len(predicted_tags) - 1):i + 1]
+                new_pos = pos_data[i - (len(predicted_tags) - 1):i + 1]
+                new_timings = timing_data[i - (len(predicted_tags) - 1):i + 1]
+                for t, w, p, tag in zip(new_timings,
+                                        new_words,
+                                        new_pos,
+                                        predicted_tags):
+                    target_file.write("\t".join([str(t[0]),
+                                                 str(t[1]),
+                                                 w,
+                                                 p,
+                                                 tag]))
+                    target_file.write("\n")
+                target_file.write("\n")
+        target_file.write("\n")
+
+
+def eval_babi(in_model,
+              in_vocab,
+              in_rev_vocab,
+              in_rev_label_vocab,
+              source_file_path,
+              in_config,
+              in_session,
+              verbose=True):
+    increco_file = 'swbd_disf_heldout_data_output_increco.text'
+    dataset = pd.read_json(source_file_path)
+    predict_babi_file(in_model,
+                      in_vocab,
+                      in_rev_vocab,
+                      in_rev_label_vocab,
+                      dataset,
+                      in_config,
+                      in_session,
+                      target_file_path=increco_file)
+    IDs, timings, words, pos_tags, labels = (map(str, range(dataset.shape[0])),
+                                       [None] * dataset.shape[0],
+                                       dataset['utterance'],
+                                       dataset['pos'],
+                                       dataset['tags']) 
+    gold_data = {}  # map from the file name to the data
+    for dialogue, a, b, c, d in zip(IDs, timings, words, pos_tags, labels):
+        current_tags_eval = convert_from_inc_disfluency_tags_to_eval_tags(d,
+                                                                          b,
+                                                                          representation="disf1")
+        # if "asr" in division and not dialogue[:4] in good_asr: continue
+        gold_data[dialogue] = (create_fake_timings(len(b)), b, c, d)
+    final_output_name = increco_file.replace("_increco", "_final")
+    incremental_output_disfluency_eval_from_file(increco_file,
+                                                 gold_data,
+                                                 utt_eval=True,
+                                                 error_analysis=False,
+                                                 word=True,
+                                                 interval=False,
+                                                 outputfilename=final_output_name)
+    # hyp_dir = experiment_dir
+    IDs, timings, words, pos_tags, labels = (map(str, range(dataset.shape[0])),
+                                       [None] * dataset.shape[0],
+                                       dataset['utterance'],
+                                       dataset['pos'],
+                                       dataset['tags'])
+    gold_data = {}  # map from the file name to the data
+    for dialogue, a, b, c, d in zip(IDs, timings, words, pos_tags, labels):
+        # if "asr" in division and not dialogue[:4] in good_asr: continue
+        current_tags_eval = convert_from_inc_disfluency_tags_to_eval_tags(d,
+                                                                          b,
+                                                                          representation="disf1")
+        d = rename_all_repairs_in_line_with_index(list(current_tags_eval))
+        gold_data[dialogue] = (create_fake_timings(len(b)), b, c, d)
+
+    # the below does just the final output evaluation, assuming a final output file, faster
+    hyp_file = "swbd_disf_heldout_data_output_final.text"
+    word = True  # world-level analyses
+    error = True  # get an error analysis
+    results, speaker_rate_dict, error_analysis = final_output_disfluency_eval_from_file(
+        hyp_file,
+        gold_data,
+        utt_eval=False,
+        error_analysis=error,
+        word=word,
+        interval=False,
+        outputfilename=None
+    )
+    # the below does incremental and final output in one, also outputting the final outputs
+    # derivable from the incremental output, takes quite a while
+    if verbose:
+        for k, v in results.items():
+            print k, v
+    all_results = deepcopy(results)
+
+    return {'f1_<rm_word': all_results['f1_<rm_word'],
+            'f1_<rps_word': all_results['f1_<rps_word'],
+            'f1_<e_word': all_results['f1_<e_word']}
+
 def filter_line(in_line, in_model, in_vocab, in_label_vocab, in_rev_label_vocab, in_session):
     tokens = in_line.lower().split()
     dataset = pd.DataFrame({'utterance': [tokens], 'tags': [['<f/>'] * len(tokens)]})
